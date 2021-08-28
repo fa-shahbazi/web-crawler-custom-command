980bc977389b1278c4351a7fa12aac362bf56f0d
root <fashahbazi2020@gmail.com>
2021-08-28 17:16:11 +0430

    change admin

--- crawler/admin.py
+++ crawler/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import Product
+
+from .models import Product, Category
 
 
-# Register your models here.
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name','product_price')
+    list_display = ('name', 'price')
+    list_filter = ('category',)
+    search_fields = ('name', 'category__title')
 
 
-admin.site.register(Product,ProductAdmin)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
