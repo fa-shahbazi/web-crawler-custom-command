429c33573431f432bccdd9a21ba10267ec10be25
root <fashahbazi2020@gmail.com>
2021-08-28 05:26:21 +0430

    Change url field

--- crawler/models.py
+++ crawler/models.py
@@ -4,26 +4,13 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Category(models.Model):
-    CATEGORIES = (
-        ("","mobile_phone"),
-        ("","cable"),
-        ("","cover"),
-        ("","headphones"),
-        ("","screen_protector"),
-        ("","computer_eqipment"),
-        ("","audiovisiual"),
-        ("","powerbank"),
-        ("","smartwatch"),
-        ("","game_console"),
-        ("","xiaomi_accessories")
-    )
-    url = models.CharField(verbose_name=_("url"), max_length=350, choices=CATEGORIES)
+    url = models.CharField(verbose_name=_("url"), max_length=350)
 
 
 class Product(models.Model):
-    product_name = models.CharField(verbose_name=_("name"), max_length=130)
-    product_price = models.CharField(verbose_name=_("price"), max_length=50)
-    product_category = models.ForeignKey(Category, verbose_name=_('product_category'), on_delete=models.CASCADE)
+    product_name = models.CharField(verbose_name=_("product_name"), max_length=130)
+    product_price = models.CharField(verbose_name=_("product_price"), max_length=50)
+    product_category = models.ForeignKey(Category, verbose_name=_('product_category'),default=1, on_delete=models.CASCADE)
     created_time = models.DateTimeField(verbose_name=_('Created time'), auto_now_add=True)
     updated_time = models.DateTimeField(verbose_name=_('updated time'), auto_now=True)
 
