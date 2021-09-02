ac35fa389404bec2ee88eac546bfc6cc78b7a8ae
root <fashahbazi2020@gmail.com>
2021-09-03 02:48:23 +0430

    Add views to crawler

--- crawler/views.py
+++ crawler/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'crawler/product_list.html', {'categories':categories})
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'crawler/product_list.html', {'products':products})
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    context = {'product': product}
+    return render(request, 'crawler/product_detail.html', context)
