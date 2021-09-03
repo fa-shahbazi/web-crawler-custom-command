b0f992aea49ba155a3ecd80c6e8f333d2d396a3b
root <fashahbazi2020@gmail.com>
2021-09-03 19:11:02 +0430

    Change crawler url

--- crawler/urls.py
+++ crawler/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 app_name = 'crawler'
 urlpatterns = [
-    path('product-list',views.product_list, name='product_list'),
-    path('product-detail/<int:product_id>/', views.product_detail, name='product_detail')
+
+    path('product-detail/<int:id>/', views.product_detail, name='product-detail'),
+    path('category-list/', views.category_list, name='category-list'),
+    path('category_product_list/<int:cat_id>/', views.category_product_list, name='category-product-list'),
+
 ]
