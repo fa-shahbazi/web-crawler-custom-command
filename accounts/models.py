2fd9e2eb2a32774bad64fcdc55d2e84a62a80ad7
root <fashahbazi2020@gmail.com>
2021-09-04 13:13:49 +0430

    Change accounts model

--- accounts/models.py
+++ accounts/models.py
@@ -5,18 +5,15 @@ from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, verbose_name=_("user"), on_delete=models.CASCADE,related_name='profile')
+    user = models.OneToOneField(User, verbose_name=_("user"), on_delete=models.CASCADE, related_name='profile')
     age = models.PositiveSmallIntegerField(_("age"), null=True, blank=True)
-    phone_number = models.PositiveSmallIntegerField(_("phone number"), unique=True, null=True, blank=True)
     bio = models.TextField(_('bio'), null=True, blank=True)
 
     class Meta:
         verbose_name = _('profiles')
         verbose_name_plural = _('profiles')
 
-    def __str__(self):
-        return self.user
-
+  
 
 def save_profile(sender, **kwargs):
     if kwargs['created']:
