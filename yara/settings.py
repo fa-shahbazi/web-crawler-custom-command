70d054d1ae24dab50d83ab02df5c95fb40e65545
root <fashahbazi2020@gmail.com>
2021-09-04 13:16:28 +0430

    Change language

--- yara/settings.py
+++ yara/settings.py
@@ -92,7 +92,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'fa'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Asia/Tehran'
 
