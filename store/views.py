b7afe700368f80d8f75241c8a5ece468ebcb43ec
root <fashahbazi2020@gmail.com>
2021-09-11 21:30:41 +0430

    Add views

--- store/views.py
+++ store/views.py
@@ -7,13 +7,18 @@ def category_product_list(request, cat_id):
     data = Product.objects.filter(category=category)
     return render(request, 'store/category_product_list.html', {'data':data})
 
+
 def category_list(request):
     categories = Category.objects.all()
     return render(request, 'store/category_list.html', {'categories':categories})
-
 
 
 def product_detail(request, id):
     product = Product.objects.get(id=id)
     context = {'product': product}
     return render(request, 'store/product_detail.html', context)
+
+
+
+
+
