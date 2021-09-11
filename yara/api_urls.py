cd1f1860a8a6e4a58a76125de897abf9754348dc
root <fashahbazi2020@gmail.com>
2021-09-11 21:32:32 +0430

    Test for url

---
+++ yara/api_urls.py
@@ -0,0 +1,7 @@
+# from rest_framework import routers
+# from store.api_views import CategoryViewset, ProductViewset
+#
+#
+# router = routers.SimpleRouter()
+# router.register('category', CategoryViewset, basename='category')
+# router.register('product', ProductViewset, basename='product')
