887e2a557148deffe302f58cfda17e67a0eda5d9
root <fashahbazi2020@gmail.com>
2021-09-11 21:21:55 +0430

    Add api urls for store

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .api_views import CategoryListApiView, CategoryDetailApiView, ProductDetailApiView, ProductListApiView
 
 app_name = 'store'
 urlpatterns = [
@@ -7,5 +8,11 @@ urlpatterns = [
     path('product-detail/<int:id>/', views.product_detail, name='product-detail'),
     path('category-list/', views.category_list, name='category-list'),
     path('category_product_list/<int:cat_id>/', views.category_product_list, name='category-product-list'),
+
+
+    path('api/category-list/', CategoryListApiView.as_view(), name='category_list'),
+    path('api/category-detail/<int:pk>/', CategoryDetailApiView.as_view(), name='category_detail'),
+    path('api/product-detail/<int:pk>/', ProductDetailApiView.as_view(), name='product_detail'),
+    path('api/product-list/', ProductListApiView.as_view(), name='product_list'),
 
 ]
