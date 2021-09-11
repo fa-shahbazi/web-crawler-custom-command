17c843c7c81b9105685437306603898271bd4d52
root <fashahbazi2020@gmail.com>
2021-09-11 21:31:10 +0430

    Change Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,20 @@
+from __future__ import unicode_literals
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from .models import Profile
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+
+from django.contrib.auth import get_user_model
+from django.contrib import admin
+
+User = get_user_model()
+
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
 
 
 class ProfileInline(admin.StackedInline):
@@ -13,6 +26,7 @@ class ProfileInline(admin.StackedInline):
 
 class ExtendedProfileAdmin(UserAdmin):
     inlines = (ProfileInline,)
+
 
 admin.site.unregister(User)
 admin.site.register(User, ExtendedProfileAdmin)
