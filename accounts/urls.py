04b3a4a1f197a0d733c1da897a13fbb8afa7e781
root <fashahbazi2020@gmail.com>
2021-09-11 21:22:30 +0430

    Add api urls for accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
+from .api_views import RegisterView, UserListCreateAPIView
 
-app_name= 'accounts'
-urlpatterns=[
+app_name = 'accounts'
+
+urlpatterns = [
     path('login/', views.user_login, name='user_login'),
     path('register/', views.user_register, name="user_register"),
     path('logout/', views.user_logout, name="user_logout"),
-
+    path('register-view/', RegisterView.as_view()),
+    path('login-view/', UserListCreateAPIView.as_view()),
+    path('confirm-register/', views.confirm_registration, name='confirm'),
 ]
