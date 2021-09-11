01bc790152d472f12b1bd15cbb1e74f5c8ff9831
root <fashahbazi2020@gmail.com>
2021-09-11 21:33:22 +0430

    Add some url

--- yara/urls.py
+++ yara/urls.py
@@ -1,10 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path, include
+# from.api_urls import router
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls', namespace='store')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
+    # path('api/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token)
 
 ]
